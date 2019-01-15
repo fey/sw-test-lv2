ce056b7b83e87d9fff485839e3235a4e8ac2cf94
fey <feycot@gmail.com>
2019-01-15 20:08:16 +0500

    phpcbf fixes

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -1,19 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * Class Book
  * Контроллер для работы с книгами
  */
-class Book extends CI_Controller {
+class Book extends CI_Controller
+{
 
-	/**
-	 * Загрузка списка книг
-	 */
-	public function loadList()
-	{
-		$this->load->model('Book_model');
-		$bookList = $this->Book_model->loadList();
-		echo json_encode($bookList);
-	}
+    /**
+     * Загрузка списка книг
+     */
+    public function loadList()
+    {
+        $this->load->model('Book_model');
+        $bookList = $this->Book_model->loadList();
+        echo json_encode($bookList);
+    }
 }
