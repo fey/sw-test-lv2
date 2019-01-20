477cb0c28ed1c209c49247f7fe4650eef4913cdc
fey <feycot@gmail.com>
2019-01-20 13:24:07 +0500

    add create book form

--- jscore/view/Books.js
+++ jscore/view/Books.js
@@ -24,12 +24,63 @@ Ext.define('Swan.view.Books', {
 		text: 'Добавить',
 		handler: function() {
 			// todo надо реализовать добавление
-			Ext.Msg.alert('В разработке', 'Данный функционал ещё не реализован');
+			Ext.create('Ext.window.Window', {
+				title: 'Добавить книгу',
+				// height: 650,
+				// width: 400,
+				items: [
+					Ext.create('Ext.form.Panel', {
+						renderTo: document.body,
+						bodyPadding: 10,
+						url: 'book/create',
+						defaultType: 'textfield',
+						items: [
+							{
+								fieldLabel: 'Автор',
+								name: 'author'
+							},
+							{
+								fieldLabel: 'Название книги',
+								name: 'name'
+							},
+							{
+								xtype: 'datefield',
+								fieldLabel: 'Год издания',
+								name: 'year'
+							}
+						],
+						buttons: [
+							{
+								text: 'Submit',
+								handler: function() {
+									const form = this.up('form'); // get the form panel
+									
+									if (form.isValid()) { // make sure the form contains valid data before submitting
+										form.submit({
+											success: function(form, action) {
+												
+											   Ext.Msg.alert('Success', action.result.msg);
+											},
+											failure: function(form, action) {
+												console.log(form, action);
+												Ext.Msg.alert('Failed', action.result.msg);
+											}
+										});
+									} else { // display error alert if the data is invalid
+										Ext.Msg.alert('Invalid Data', 'Please correct form errors.')
+									}
+								}
+							}
+						]
+					}),
+				]
+			}).show();
 		}
 	}, {
 		text: 'Редактировать',
-		handler: function() {
+		handler: function () {
 			// todo надо реализовать редактирование
+			console.error(this.text);
 			Ext.Msg.alert('В разработке', 'Данный функционал ещё не реализован');
 		}
 	}, {
